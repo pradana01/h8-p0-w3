9e822ef3f5105ca8c8dd525d723f466d71e107b7
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-03-06 20:09:21 +0700

    commit push
    revisi

--- exercise06.js
+++ exercise06.js
@@ -1,27 +1,3 @@
-/*function angkaPalindrome(num){
-    if (num < 9){
-        num++;
-    
-    } else {
-            var angka1 = '1';
-            var angka2 = '3';
-            var str = num.toString();
-            while (angka1 !== angka2){    
-                str = num.toString();
-                angka1 = '';
-                angka2 = '';
-                for (var i = 0; i < str.length; i++){
-                    angka1 += str[i];
-                }
-                for (var j = str.length - 1; j>= 0; j--){
-                    angka2 += str[j]
-                }
-                num++;
-            } 
-        } 
-    return num;
-}*/
-
 function angkaPalindrome(num){
     if (num < 9){
         num++
